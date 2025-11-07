c02819b0fb42818408899bd6c683919a46593718
Caio Borsato Alonso <caioca827@gmail.com>
2025-11-07 11:17:16 -0300

    Update UsuarioController.java

--- src/main/java/senai/api/projetoApi/controllers/UsuarioController.java
+++ src/main/java/senai/api/projetoApi/controllers/UsuarioController.java
@@ -2,7 +2,6 @@ package senai.api.projetoApi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import senai.api.projetoApi.models.Usuario;
 import senai.api.projetoApi.services.UsuarioService;
 
@@ -21,22 +20,15 @@ public class UsuarioController {
             @RequestParam("senha") String senha,
             @RequestParam("confirmarSenha") String confirmarSenha) {
         
-        // 1. Criamos um objeto Usuario manualmente com os parâmetros
         Usuario usuario = new Usuario();
         usuario.setNome(nome);
         usuario.setEmail(email);
         usuario.setCpf(cpf);
         usuario.setSenha(senha);
 
-        // 2. Enviamos o usuário e a confirmação para o serviço
-        // O serviço fará a validação de senha e de duplicidade
         return usuarioService.cadastrar(usuario, confirmarSenha);
     }
 
-    /**
-     * O login foi mantido com @RequestBody,
-     * pois é o método mais comum e seguro para login.
-     */
     @PostMapping("/login")
     public String login(@RequestBody Usuario usuario) {
         boolean autenticado = usuarioService.login(usuario.getEmail(), usuario.getSenha());
