7967f91e4a4778eed79d669332ac0bcc40e1e97c
Caio Borsato Alonso <caioca827@gmail.com>
2025-11-10 09:40:55 -0300

    Update Swagger.java

--- src/main/java/senai/api/projetoApi/configs/Swagger.java
+++ src/main/java/senai/api/projetoApi/configs/Swagger.java
@@ -1,5 +1,16 @@
-package senai.api.projetoApi.configs;
+import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Shop",
+        version = "1.0",
+        description = "Caio Borsato,Otavio,Marlon,Miguel,Thiago e Pedro Vogel"
+    )
+)
 public class Swagger {
 
 }
