c819699134781f8d631ee254ee40d1143999ac35
Caio Borsato Alonso <caioca827@gmail.com>
2025-11-20 20:56:42 -0300

    Add files via upload

--- src/main/java/com/senai/infoa/Api/Joao/repositories/ReservaRepository.java
+++ src/main/java/com/senai/infoa/Api/Joao/repositories/ReservaRepository.java
@@ -1,6 +1,10 @@
 package com.senai.infoa.Api.Joao.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.senai.infoa.Api.Joao.models.Reserva;
@@ -8,4 +12,8 @@ import com.senai.infoa.Api.Joao.models.Reserva;
 @Repository
 public interface  ReservaRepository extends JpaRepository<Reserva, Integer>{
     
-}
+    @Query(value="SELECT * FROM reserva WHERE usuario_id = :id",nativeQuery=true)
+    public List<Reserva> BuscarReservasUsuario (@Param("id")Integer id);
+
+    }
+
