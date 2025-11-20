414442499babc546bd4e3326a34b97e007dad13d
Caio Borsato Alonso <caioca827@gmail.com>
2025-11-19 21:46:21 -0300

    Add files via upload

--- src/main/java/com/senai/infoa/Api/Joao/models/Equipamentos.java
+++ src/main/java/com/senai/infoa/Api/Joao/models/Equipamentos.java
@@ -10,12 +10,10 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Data
 @Builder
 @Entity
 @AllArgsConstructor
