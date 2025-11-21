0c58dd8f67762a869c5ea0a21ea8fb04b2b04248
Caio Borsato Alonso <caioca827@gmail.com>
2025-11-20 21:58:55 -0300

    Update Equipamentos.java

--- src/main/java/com/senai/infoa/Api/Joao/models/Equipamentos.java
+++ src/main/java/com/senai/infoa/Api/Joao/models/Equipamentos.java
@@ -1,12 +1,11 @@
 package com.senai.infoa.Api.Joao.models;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -22,7 +21,7 @@ public class Equipamentos {
     @Column(name="nome_equipamento")
     private String nomeEquipamento;
 
-    @OneToOne(mappedBy="equipamentos")
+    @OneToMany(mappedBy="equipamentos")
     private Reserva reserva;
 
     public Equipamentos() {
@@ -63,4 +62,3 @@ public class Equipamentos {
     
 
 }
-
