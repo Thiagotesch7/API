a81e83c4e09d29ae4f2362df32cef4500a20f94e
Caio Borsato Alonso <caioca827@gmail.com>
2025-11-20 21:57:55 -0300

    Update Reserva.java

--- src/main/java/com/senai/infoa/Api/Joao/models/Reserva.java
+++ src/main/java/com/senai/infoa/Api/Joao/models/Reserva.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -26,11 +26,11 @@ public class Reserva {
     private LocalDate dataFinal;
 
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name="usuario_id")
     private Usuario usuario;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name="equipamento_id")
     private Equipamentos equipamentos;
 
