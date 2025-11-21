938b939dc5d246f69a4117488e2218b2e406bca9
Caio Borsato Alonso <caioca827@gmail.com>
2025-11-20 21:06:30 -0300

    Update ReservaController.java

--- src/main/java/com/senai/infoa/Api/Joao/Controller/ReservaController.java
+++ src/main/java/com/senai/infoa/Api/Joao/Controller/ReservaController.java
@@ -1,10 +1,14 @@
 package com.senai.infoa.Api.Joao.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.senai.infoa.Api.Joao.Services.ReservaService;
@@ -24,10 +28,10 @@ public class ReservaController {
         return ResponseEntity.ok("Reserva Feita Com Sucesso!!");
     }
 
-   // @GetMapping("/ListarReservaUsuario")
-   // public List<Reserva> listarResercaDoUsuario(@RequestParam Integer usuario_id) {
-     //   return reservaService.ListarReservasUsuario(usuario_id);
-    
+   @GetMapping("/ListarReservaUsuario")
+   public List<Reserva> listarResercaDoUsuario(@RequestParam Integer id) {
+    return reservaService.ListarReservasUsuario(id);
+    }
     
     //@GetMapping("/ListarReservaEquipamento")
     //public List<Reserva> listarReservaDoEquipamento(@RequestParam Integer equipamento_id) {
